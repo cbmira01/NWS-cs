67eb625470cfa7461c8da7a7f0588c5ff83b54de
Calvin Miracle <cbmira01@gmail.com>
2016-10-16 21:03:58 -0400

    Giving up on SQL data load.

--- NWS-cs/Global.asax.cs
+++ NWS-cs/Global.asax.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NWS_cs.Migrations;
 
 namespace NWS_cs
 {
--- NWS-cs/Migrations/Configuration.cs
+++ NWS-cs/Migrations/Configuration.cs
@@ -16,17 +16,6 @@ namespace NWS_cs.Migrations
         protected override void Seed(NWS_cs.NwsContext context)
         {
 
-            string line;
-
-            // Read the file and display it line by line.
-            System.IO.StreamReader file = new System.IO.StreamReader("NwsSeedData.sql");
-            while ((line = file.ReadLine()) != null)
-            {
-                context.Database.ExecuteSqlCommand(line);
-            }
-
-            context.SaveChanges();
-            file.Close();
         }
     }
 }
