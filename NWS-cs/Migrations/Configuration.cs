84c6228afba88901996a4fe36af23002b416391b
Calvin Miracle <cbmira01@gmail.com>
2016-10-18 15:58:46 -0400

    Trying to get database seeding just right.

--- NWS-cs/Migrations/Configuration.cs
+++ NWS-cs/Migrations/Configuration.cs
@@ -1,16 +1,13 @@
 namespace NWS_cs.Migrations
 {
-    using System;
     using System.Collections.Generic;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<NWS_cs.NwsContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(NWS_cs.NwsContext context)
@@ -224,3 +221,4 @@ namespace NWS_cs.Migrations
         }
     }
 }
+
