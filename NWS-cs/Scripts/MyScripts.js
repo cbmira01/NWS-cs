37b214cce1d9403a6dce2e7cb98343dec63b0213
Calvin Miracle <cbmira01@gmail.com>
2016-10-23 17:02:32 -0400

    Youtube embeds work with static placement.

--- NWS-cs/Scripts/MyScripts.js
+++ NWS-cs/Scripts/MyScripts.js
@@ -1,16 +1,26 @@
 ﻿
-// Initialize Zurb Foundation
-$(document).foundation();
+// Proper Zurb Foundation initialization, thanks to http://stackoverflow.com/a/32790543
+$(document).ready(function () {
+    $(document).foundation({
+        equalizer: {
+            // Specify if Equalizer should make elements equal height once they become stacked.
+            equalize_on_stack: true
+        }
+    });
+});
+
 
 // Initialize Slick Carousel
 $(document).ready(function () {
     $('.featureCarousel').slick({
         accessibility: true,
-        dots: false,
-        infinite: true,
+        arrows: true,
         autoplay: true,
         autoplayspeed: 3000,
+        cssEase: 'linear',
+        dots: true,
+        infinite: true,
         fade: true,
-        cssEase: 'linear'
+        mobileFirst: true
     });
 });
