fc82289f87c004247f510ce408a5b7b78537b0d4
Calvin Miracle <cbmira01@gmail.com>
2016-10-23 13:54:52 -0400

    Feature Carousel works after jQuery support installed at highest v2 release.

---
+++ NWS-cs/Scripts/MyScripts.js
@@ -0,0 +1,16 @@
+﻿
+// Initialize Zurb Foundation
+$(document).foundation();
+
+// Initialize Slick Carousel
+$(document).ready(function () {
+    $('.featureCarousel').slick({
+        accessibility: true,
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplayspeed: 3000,
+        fade: true,
+        cssEase: 'linear'
+    });
+});
