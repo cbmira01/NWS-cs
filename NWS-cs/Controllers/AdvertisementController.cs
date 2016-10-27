738afb9aaed686acc3436813e3b317e3b757b8ad
Calvin Miracle <cbmira01@gmail.com>
2016-10-27 13:51:22 -0400

    Ad edit now styled.

--- NWS-cs/Controllers/AdvertisementController.cs
+++ NWS-cs/Controllers/AdvertisementController.cs
@@ -58,10 +58,8 @@ namespace NWS_cs.Controllers
         }
 
         // POST: Advertisement/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "id,link")] Advertisement advertisement)
         {
             if (ModelState.IsValid)
