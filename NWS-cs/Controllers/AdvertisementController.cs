91b3552817045af7f4a8596994bd6ea42f7d774a
Calvin Miracle <cbmira01@gmail.com>
2016-10-27 16:16:56 -0400

    Ad create is now styled.

--- NWS-cs/Controllers/AdvertisementController.cs
+++ NWS-cs/Controllers/AdvertisementController.cs
@@ -26,10 +26,7 @@ namespace NWS_cs.Controllers
         }
 
         // POST: Advertisement/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "id,link")] Advertisement advertisement)
         {
             if (ModelState.IsValid)
