d1064f4429b776be585c405ab5f84bd4092bb4da
Calvin Miracle <cbmira01@gmail.com>
2016-10-28 22:39:17 -0400

    Media-responsive styling of top-bar now works.

--- NWS-cs/Scripts/MyScripts.js
+++ NWS-cs/Scripts/MyScripts.js
@@ -1,16 +1,38 @@
-﻿
-// Proper Zurb Foundation initialization, thanks to http://stackoverflow.com/a/32790543
+﻿"use strict";
+
+// Reactive feature for small media: restyle header bar.
+$(window).on("load resize", function (e) {
+    var dpnaElement = document.getElementById("dpnaText");
+
+    if (dpnaElement === null) { return }
+
+    if (Modernizr.mq('(max-width: 40em)')) { // if media is small-only
+        dpnaElement.innerHTML = "DPNA";
+        $(document).foundation({
+            equalizer : { equalize_on_stack: false }
+        });
+    }
+    else {
+        dpnaElement.innerHTML = "Deer Park Neighborhood Association";
+        $(document).foundation({
+            equalizer : { equalize_on_stack: true }
+        });
+    }
+});  //end on-resize
+  
+
+// Zurb foundation initialization, http://foundation.zurb.com/sites/docs/v/5.5.3/components/equalizer.html
 $(document).ready(function () {
     $(document).foundation({
         equalizer: {
-            // Specify if Equalizer should make elements equal height once they become stacked.
+            // specify if equalizer should make elements equal height once they become stacked.
             equalize_on_stack: true
         }
     });
 });
 
 
-// Initialize Slick Carousel
+// Initialize Slick Carousel.
 $(document).ready(function () {
     $('.featureCarousel').slick({
         accessibility: true,
