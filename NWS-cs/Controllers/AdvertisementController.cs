07516e1985910c75e342a6fa21c24165833603e0
Calvin Miracle <cbmira01@gmail.com>
2016-11-01 20:37:03 -0400

    Added Edit content buttons to Ad Content view.

--- NWS-cs/Controllers/AdvertisementController.cs
+++ NWS-cs/Controllers/AdvertisementController.cs
@@ -14,6 +14,11 @@ namespace NWS_cs.Controllers
     {
         private NwsContext db = new NwsContext();
 
+        public ActionResult _EditButtonPartial()
+        {
+            return PartialView();
+        }
+
         public ActionResult Content()
         {
             return View(db.Advertisements.ToList());
