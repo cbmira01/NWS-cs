76828f1247b1d87afb7e5c3838e7565bba22663e
Calvin Miracle <cbmira01@gmail.com>
2016-11-06 08:18:35 -0500

    Created an exploratory console app to deserialize the Jeopardy dataset.

---
+++ NWS-Console/JeopardyRecord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NWS_Console
+{
+    // This is what an eventual Entity Framework model
+    // would look like, for the main web application.
+    class JeopardyRecord
+    {
+        public string category { get; set; }
+        public string air_date { get; set; }
+        public string question { get; set; }
+        public string value { get; set; }
+        public string answer { get; set; }
+        public string round { get; set; }
+        public string show_number { get; set; }
+    }
+}
--- NWS-Console/Program.cs
+++ NWS-Console/Program.cs
@@ -1,42 +1,130 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace NWS_Console
 {
     class Program
     {
+        //
+        // The code in this solution is basically disposable. This program helped with:
+        //      - exploration of the Jeopardy database 
+        //        https://www.reddit.com/r/datasets/comments/1uyd0t/200000_jeopardy_questions_in_a_json_file/
+        //      - inter-solution relative path references
+        //      - set up and use of NewtonSoft JSON library
+        //
+        // The Jeopardy file is a array of objects, each object describing a particular
+        // historical Jeopardy question, its category, answer, and show date. See the
+        // JeopardyRecord class for the complete model.
         static void Main(string[] args)
         {
-            string path = @"c:\temp\q1.json";
+            ConvertRawToLineFormat();
+            UnserializeJson();
+        }
+
+        // Demonstrate proper use of NewtonSoft JSON library, and find all fields of
+        // the Jeopardy records.
+        public static void UnserializeJson()
+        {
+            string solutionPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string sourceFile = solutionPath + @"\Data\JeopardyDataProcessed.json";
+
+            string[] jsonLines = File.ReadAllLines(sourceFile);
+            string jsonLine;
+
+            Console.WriteLine();
+            Console.WriteLine("{0} Jeopardy records found; <CR> to continue.", jsonLines.Length);
+            Console.ReadLine();
+
+            for (int i = 0; i < jsonLines.Length; i++)
+            {
+                jsonLine = jsonLines[i];
+
+                // Let's see all URLs and check any that DO NOT point to j-archive.com
+                if (!jsonLine.Contains("http"))
+                {
+                    continue;
+                }
+
+                if (jsonLine.Contains("j-archive.com"))
+                {
+                    continue;
+                }
+
+                JeopardyRecord jr = JsonConvert.DeserializeObject<JeopardyRecord>(jsonLine);
+
+                Console.WriteLine("Item {0}", i);
+                Console.WriteLine(jr.category);
+                Console.WriteLine(jr.air_date);
+                Console.WriteLine(jr.question);
+                Console.WriteLine(jr.value);
+                Console.WriteLine(jr.answer);
+                Console.WriteLine(jr.round);
+                Console.WriteLine(jr.show_number);
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+
+
+        // Convert the raw Jeopardy records into CRLF-terminated records,
+        // and write back to a destination file suitable for use by the 
+        // web-app migration/seeding.
+        public static void ConvertRawToLineFormat()
+        {
+            string solutionPath = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            string sourceFile = solutionPath + @"\Data\JeopardyDataRaw.json";
+            string destFile = solutionPath + @"\Data\JeopardyDataProcessed.json";
+
+            int count = 0;
 
             try
             {
-                using (StreamReader sr = new StreamReader(path))
+                using (StreamReader sr = new StreamReader(sourceFile))
+                using (StreamWriter sw = new StreamWriter(destFile))
                 {
                     char c;
+                    StringBuilder sb = new StringBuilder();
 
                     while (sr.Peek() >= 0)
                     {
+                        // Look for an opening curly-bracket
                         c = (char)sr.Read();
+                        if (c != '{')
+                            continue;
 
-                        Console.Write(c);
+                        sb.Clear();
+                        sb.Append(c);
 
-                        if (c == '}')
+                        //Now inside JSON curly brackets
+                        while (true)
                         {
-                            Console.WriteLine();
+                            c = (char)sr.Read();
+                            sb.Append(c);
 
+                            if (c == '}')
+                                break;
                         }
+
+                        // JSON object recognized; store it
+                        count++;
+                        if (count % 1000 == 0)
+                            Console.Write(".");
+
+                        sw.WriteLine(sb.ToString());
                     }
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("The process failed: {0}", e.ToString());
+                Console.WriteLine("ConvertRawToLineFormat failed: {0}", e.ToString());
             }
-        }
+
+        } // end ConvertRawToLineFormat
+
+
     }
 }
+
